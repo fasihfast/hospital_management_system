27815462c6a8a9f66ea2ab811a1522f33b107e6f
fasihfast <muhammadfasihdj@gmail.com>
2025-04-24 23:10:11 +0500

    Initial commit with Routers, Controllers, and Model folders

---
+++ controllers/messageController.js
@@ -0,0 +1,22 @@
+import { Message } from '../models/messageSchema.js'; // adjust path if needed
+import {catchAsyncErrors} from "../middlewares/catchAsyncError.js"
+
+export const sendMessage = catchAsyncErrors(async (req, res, next) => {
+    console.log(`Request body : ${req.body}`)
+    const { firstName, lastName, email, phone, message } = req.body;
+
+    if (!firstName || !lastName || !email || !phone || !message) {
+        return res.status(400).json({
+            success: false,
+            message: "Please Enter Complete details"
+        });
+    }
+
+        await Message.create({ firstName, lastName, email, phone, message });
+
+        res.status(200).json({
+            success: true,
+            message: "Message sent successfully"
+        });
+    
+});
---
+++ models/messageSchema.js
@@ -0,0 +1,41 @@
+import mongoose from 'mongoose'
+import validator from 'validator'
+
+const messageSchema= new mongoose.Schema({
+    firstName:{
+        type:String,
+        required:true,
+        minLength:[3,"Number of Characters should be >= 3"]
+    },
+
+    lastName:{
+        type:String,
+        required:true,
+        minLength:[3,"Number of Characters should be >= 3"]
+    },
+    email:{
+        type:String,
+        required:true,
+        validate:[validator.isEmail,"Invalid email address"]
+    },
+
+    phone: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                // Ensures that the phone number is exactly 11 digits long
+                return /^\d{11}$/.test(v);
+            },
+            message: "Phone number should consist of exactly 11 digits"
+        }
+    },
+
+    message:{
+        type: String,
+        required:true,
+        minLength:[10,"Min characters should be 10"]
+    }
+})
+
+export const Message=mongoose.model("Message",messageSchema);
